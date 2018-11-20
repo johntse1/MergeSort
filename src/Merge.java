b1f557ccb33f817b0325aa226e068a2919f0e8d4
BT_1N3_03 <@>
2018-11-20 10:13:40 -0500

    Initial commit

---
+++ src/Merge.java
@@ -0,0 +1,23 @@
+public class Merge
+{
+    public void mergeSort(int[] arr)
+    {
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+    public void mergeSortHelper(int[] arr,int left, int right, int[] temp)
+    {
+        if(left<right)
+        {
+            int mid = (left+right) /2;
+            mergeSortHelper(arr, left, right, temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr, left, mid, right, temp);
+        }
+    }
+    public void merge(int[] arr, int left, int mid, int right, int[] temp)
+    {
+        
+    }
+}
